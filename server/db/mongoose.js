2484b246a5fb1430b9561b21bca2e84693466cd7
steve_breese <steve.breese@hs2solutions.com>
2017-07-31 16:59:36 -0500

    Cool Goolgle, cool

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 });
 
--- server/server.js
+++ server/server.js
@@ -13,11 +13,12 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+
+todo.save().then((doc) => {
+  res.send(doc);
+}, (e) => {
+  res.status(400).send(e);
+});
 });
 
 app.get('/todos', (req, res) => {
@@ -28,10 +29,8 @@ app.get('/todos', (req, res) => {
 });
 });
 
-
-
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
