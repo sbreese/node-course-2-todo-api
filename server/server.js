66ac10ace25ab35f659111a12564ba7ca3ba68a6
steve_breese <steve.breese@hs2solutions.com>
2017-08-05 12:48:50 -0500

    google please hire me

--- server/server.js
+++ server/server.js
@@ -16,55 +16,55 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+todo.save().then((doc) => {
+  res.send(doc);
+}, (e) => {
+  res.status(400).send(e);
+});
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  res.send({todos});
+}, (e) => {
+  res.status(400).send(e);
+});
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
 
-  Todo.findById(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+  return res.status(404).send();
+}
 
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
-  });
+res.send({todo});
+}).catch((e) => {
+  res.status(400).send();
+});
 });
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
 
-  Todo.findByIdAndRemove(id).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
+Todo.findByIdAndRemove(id).then((todo) => {
+  if (!todo) {
+  return res.status(404).send();
+}
 
-    res.send(todo);
-  }).catch((e) => {
-    res.status(400).send();
-  });
+res.send({todo});
+}).catch((e) => {
+  res.status(400).send();
+});
 });
 
 app.listen(port, () => {
