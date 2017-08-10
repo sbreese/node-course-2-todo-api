a6a2cd289a0d96202fd9dff4050867cf32426fa6
steve_breese <steve.breese@hs2solutions.com>
2017-08-10 17:20:55 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -11,7 +11,10 @@ var UserSchema = new mongoose.Schema({
     minlength: 1,
     unique: true,
     validate: {
-      validator: validator.isEmail,
+      //validator: validator.isEmail,
+      validator: (value)=>{
+      return validator.isEmail(value);
+    },
       message: '{VALUE} is not a valid email'
     }
   },
@@ -49,6 +52,23 @@ UserSchema.methods.generateAuthToken = function () {
   return user.save().then(() => {
       return token;
 });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 };
 
 var User = mongoose.model('User', UserSchema);
